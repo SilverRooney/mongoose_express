dc3eb91f50c37fcf0981e7be030b71e10765078f
silver <silver.rooney.dev@gmail.com>
2022-03-09 10:35:54 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(() => {
+        console.log("MONGOConnection Open!!!");
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!");
+        console.log(err);
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products });
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.send('details page!');
+})
+
+app.listen(3000, () => {
+    console.log('APP IS LISTENING ON PORT 3000!');
+})
